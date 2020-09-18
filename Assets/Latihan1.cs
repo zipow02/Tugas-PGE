3ab9118c2accb3e22ac1f0065cd406db7572995c
zipow02 <zivahanifa461@gmail.com>
2020-09-18 23:07:34 +0700

    Pertemuan ke-2
    Upload Asset dan Project Setting

---
+++ Assets/Latihan1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour 
+{
+	// Start is called before the first frame update
+	void Start() 
+	{
+		Debug.Log("Hello Dicoding!");
+	}
+
+	// Update is called once per frame
+	void Update() 
+	{
+
+	}
+}
