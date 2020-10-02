8f5801dd63bd5c867046160ebfaaac382b547cbc
zipow02 <zivahanifa461@gmail.com>
2020-10-02 19:21:36 +0700

    Pertemuan ke-4
    Pertemuan ke-4

---
+++ Assets/Latihan5/Prajurit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prajurit : Manusia 
+{
+	void Start () 
+	{
+		Debug.Log ("Seorang Prajurit dapat : ");
+		Makan ();
+		Tidur ();
+		Menyerang ();
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	void Menyerang()
+	{
+	Debug.Log ("Menyerang");
+	}
+}
