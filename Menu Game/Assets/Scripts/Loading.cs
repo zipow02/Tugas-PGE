2b11151236c487193bb844775c4a59079e894432
zipow02 <zivahanifa461@gmail.com>
2020-10-11 00:07:17 +0700

    Pertemuan ke-5
    Pertemuan ke-5

---
+++ Menu Game/Assets/Scripts/Loading.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour {
+	
+	// waktu delay untuk load scene
+	[SerializeField] private float delayLoading = 3f;
+
+	// nama scene untuk pindah scene yang diinputkan pada Inspector
+			[SerializeField] private string namaScene;
+
+	private float timeElapsed;
+
+	private void Update() {
+		timeElapsed += Time.deltaTime;
+
+		if (timeElapsed > delayLoading) {
+			SceneManager.LoadScene (namaScene);
+		}
+	}
+}
---
+++ Menu Game/Assets/Scripts/Scene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+	//pindah scene ke menu
+	public void MenuScene() {
+		SceneManager.LoadScene ("Menu");
+	}
+
+	// pindah scene ke loading
+	public void LoadingScene() {
+		SceneManager.LoadScene ("Loading");
+	}
+
+	// keluar dari game
+	public void KeluarScene () {
+		Application.Quit ();
+	}
+}
