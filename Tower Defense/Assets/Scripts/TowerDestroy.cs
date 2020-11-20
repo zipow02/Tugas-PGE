01573f11ad0a7fbb17475d5d90b1487be69a4a09
zipow02 <zivahanifa461@gmail.com>
2020-11-20 17:11:23 +0700

    Pertemuan ke-9
    Pertemuan ke-9

---
+++ Tower Defense/Assets/Scripts/TowerDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDestroy : MonoBehaviour 
+{
+	public bool isPlayer = true;
+	private void OnDestroy()
+	{
+		if (!Data.isGameOver)
+		if (isPlayer)
+		{
+			Data.isGameOver = true;
+			Data.isComplete = false;
+			Debug.Log("Player Lost");
+		}
+		else
+		{
+			Data.isGameOver = true;
+			Data.isComplete = true;
+			Debug.Log("Player Win");
+		}
+	}
+}
