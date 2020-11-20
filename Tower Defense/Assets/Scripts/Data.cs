06d3e33d6213a03d87b015bd4f682012e326830d
zipow02 <zivahanifa461@gmail.com>
2020-11-20 18:06:25 +0700

    Pertemuan ke-9
    Pertemuan ke-9

---
+++ Tower Defense/Assets/Scripts/Data.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+	public static float coin = 300;
+	public static bool isGameOver = false;
+	public static bool isComplete = false;
+}
+	
---
+++ Tower Defense/Assets/Scripts/Defender.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour 
+{
+	public bool isPlayer = true;
+	private bool isDefend = false;
+	public int defense = 300;
+	[HideInInspector]
+	public int underAttack;
+	private float timer = 0;
+	private string nameTagLawan;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (isPlayer) 
+		{
+			nameTagLawan = "Enemy";
+		} 
+		else 
+		{
+			nameTagLawan = "Player";
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (isDefend) 
+		{
+			//attack
+			timer += Time.deltaTime;
+			if (timer > 0.6f) 
+			{
+				defense -= underAttack;
+				timer = 0;
+			}
+		}
+		if (defense <= 0)
+		{
+			Destroy (gameObject);
+		}
+		if (transform.position.x > 9 || transform.position.x < -9) 
+		{
+			Destroy (gameObject);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.transform.tag.Equals (nameTagLawan)) 
+		{
+			isDefend = true;
+			Attacker m = collision.gameObject.GetComponent<Attacker> ();
+			if (m != null) m.underAttack = 0;
+			Defender d = collision.gameObject.GetComponent<Defender> ();
+			if (d != null) d.underAttack = 0;
+		}
+	}
+
+	public void OnCollisionExit2D(Collision2D collision)
+	{
+		isDefend = false;
+	}
+}
